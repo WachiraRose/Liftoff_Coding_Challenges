ea2c2686aecf1fc768e2045fea36c73c287f7e24
WachiraRose <laurose20@gmail.com>
2023-08-16 15:09:24 -0500

    Challenge 5: Older than me complete

--- Week5_Challenge.js
+++ Week5_Challenge.js
@@ -1,72 +1,31 @@
-/*Studio: Counting Characters
-In this studio, you will write a program to count the number of times each character occurs in a string and then print the results to the console.
+/* Older than Me
+    Create a method in the Person class which returns how another person's age compares. 
+    Given the instances p1, p2 and p3, which will be initialised with the attributes name and age, return a sentence in the following format:
+            {other person name} is {older than / younger than / the same age as} me.*/
 
-Feel free to prompt the user for a string. However, for the sake of simplicity, you might want to start by hard-coding some text and storing it in a variable. 
-For your convenience, here is a quote from the movie Hidden Figures:
 
-If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. 
-So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. 
-Once you’ve done that, it’s pretty straightforward from there.*/
-
-function countCharacters(inputString) {
-    const characterCounts = {};
-  
-    for (const char of inputString) {
-      if (characterCounts[char]) {
-        characterCounts[char]++;
+class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  // method to answer question
+    compareAge(otherPerson) {
+      if (otherPerson.age > this.age) {
+        return `${otherPerson.name} is older than me.`;
+      } else if (otherPerson.age < this.age) {
+        return `${otherPerson.name} is younger than me.`;
       } else {
-        characterCounts[char] = 1;
+        return `${otherPerson.name} is the same age as me.`;
       }
     }
-  
-    const sortedCharacters = Object.keys(characterCounts).sort();
-    for (const char of sortedCharacters) {
-      console.log(`${char}: ${characterCounts[char]}`);
-    }
   }
   
-  const text = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+  const p1 = new Person("Samuel", 24);
+  const p2 = new Person("Joel", 36);
+  const p3 = new Person("Lily", 24);
   
-  countCharacters(text);
-
-  //BONUS MISSION
-  /* 1.Prompt the user to enter the string in the command line.
-
-     2. Make the character counts case-insensitive.
-
-     3. Exclude non-alphabetic characters.*/
-
-
-  const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-function countCharacters(inputString) {
-  const characterCounts = {};
-
-  const cleanString = inputString.replace(/[^a-zA-Z]/g, '').toLowerCase();
-
-  for (const char of cleanString) {
-    if (characterCounts[char]) {
-      characterCounts[char]++;
-    } else {
-      characterCounts[char] = 1;
-    }
-  }
-
-  const sortedCharacters = Object.keys(characterCounts).sort();
-  for (const char of sortedCharacters) {
-    console.log(`${char}: ${characterCounts[char]}`);
-  }
-}
-
-rl.question('Enter a string: ', (answer) => {
-  countCharacters(answer);
-  rl.close();
-});
-
+  console.log(p1.compareAge(p2)); // Output: "Joel is older than me."
+  console.log(p2.compareAge(p1)); // Output: "Samuel is younger than me."
+  console.log(p1.compareAge(p3)); // Output: "Lily is the same age as me."
   
-
