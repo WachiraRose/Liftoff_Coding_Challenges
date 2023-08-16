3c063929451b33ff48c96d422d124d290d51697d
WachiraRose <laurose20@gmail.com>
2023-08-16 14:25:06 -0500

    Challenge 4: Add up the Numbers from a Single Number complete

--- Week4_Challenge.js
+++ Week4_Challenge.js
@@ -1,1 +1,19 @@
+/*Create a function that takes a number as an argument. 
+Add up all the numbers from 1 to the number you passed to the function. 
+For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.*/
+
+function sumUpToNumber(number) {
+    if (number < 1 || number > 1000) {
+      return "Input must be a positive number between 1 and 1000.";
+    }
+  
+    let sum = 0;
+    for (let i = 1; i <= number; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+
+  
+
 
